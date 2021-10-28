3a23ef923ecd6040ac75fa62ffc4342e573b02ac
Greg Zancewicz <greg.zancewicz@gmail.com>
2021-10-28 13:50:51 +0000

    Got car markers to show

--- src/App.js
+++ src/App.js
@@ -10,11 +10,36 @@ import {
 } from "react-bootstrap";
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps"
+
+import * as carData from "./data/car-locations.json";
+
+const GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAhEBULbR-9Y-GpafIc_twpyNMDCqXn8R0'
+const APIURL = "https://6174019a110a740017223221.mockapi.io/fleet"
+
+function StatusIcon(car) {
+  if (car.status === "Idle") {
+    return ("http://maps.google.com/mapfiles/ms/icons/yellow-dot.png")
+  } else {
+    if (car.status === "En Route") {
+      return ("http://maps.google.com/mapfiles/ms/icons/green-dot.png")
+    } else {
+      return ("http://maps.google.com/mapfiles/ms/icons/red-dot.png")
+    }
+  }
+}
+
 
 
 function App() {
 
-  const APIURL = "https://6172edab110a740017222edc.mockapi.io/fleet"
+ 
 
   const [cars, setCars] = useState([])
   const [modalInfo, setModalInfo] = useState([])
@@ -58,7 +83,9 @@ function App() {
 
   const columns = [
     { dataField: "vin", text: "Car VIN" },
-    { dataField: "status", text: "Status"}
+    { dataField: "status", text: "Status" },
+    { dataField: "lat", text: "lat" },
+    { dataField: "lon", text: "lon" }
   ]
 
   const rowEvents = {
@@ -97,26 +124,64 @@ function App() {
     )
   }
 
+
+  function Map() {
+
+
+      return (
+      <GoogleMap defaultZoom={16}
+          defaultCenter={{ lat: 33.020028, lng: -96.704167 }}
+      >
+        {Array.from(cars).map(car => (
+          <Marker
+            key={car.vin}
+            position={{
+              lat: Number(car.lat),
+              lng: Number(car.lon)
+            }}
+            icon={{
+              url: StatusIcon(car)
+            }}
+        
+          />
+        ))}
+
+        
+      </GoogleMap>
+    )
+
+  }
+
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
+
   return (
-    <div className="App">
+    <div className="App" style={{ padding: '20px' }}>
       <Container>
+        <Row></Row>
         <Row>
           <Col>
             <Row>
-            <h3>Car Status</h3>
+
             </Row>
             <Row>
-            <BootstrapTable
-              keyField="vin"
-              data={cars}
-              columns={columns}
-              pagination={paginationFactory()}
-              rowEvents={rowEvents}
-            />
+              <BootstrapTable
+                keyField="vin"
+                data={cars}
+                columns={columns}
+                pagination={paginationFactory()}
+                rowEvents={rowEvents}
+              />
             </Row>
-            { show ? <ModalContent /> : null }
+            {show ? <ModalContent /> : null}
           </Col>
-          <Col></Col>
+          <Col>
+            <div >
+              <WrappedMap
+                googleMapURL={GOOGLE_MAPS_URL}
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              /></div></Col>
         </Row>
       </Container>
     </div>
